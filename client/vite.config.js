1455f90702323d083f7eaa18642ca41c031ecdac
Nelliosis <jcapawing@gmail.com>
2024-03-16 00:05:09 +0800

    init dockerfiles for vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-})
+  preview: {
+    port: 8080,
+    strictPort: false,
+  },
+  server: {
+    port: 8080,
+    strictPort: false,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+  },
+});
